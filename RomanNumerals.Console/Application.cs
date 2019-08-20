706f8c807fdbb5bf46e4418f03004fd87466c43a
Henrik Ilgen <henrik.ilgen@acando.de>
2019-08-20 15:31:08 +0200

    Added console project

---
+++ RomanNumerals.Console/Application.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace RomanNumerals.Console {
+    using Console = System.Console;
+
+    class Application {
+        private readonly INumeralConverter numeralConverter;
+
+        public Application(
+            INumeralConverter numeralConverter
+        ) {
+            this.numeralConverter = numeralConverter;
+        }
+
+        internal int Run(int? number) {
+            try {
+                Console.WriteLine(
+                    numeralConverter.ToNumeral(
+                        number ?? Int32.Parse(Console.In.ReadToEnd())
+                    )
+                );
+                return 0;
+            } catch (ArgumentException) {
+                Console.Error.WriteLine($"Could not format number {number} to numeral. "
+                    + "See --help for details on which numbers can be formatted.");
+                return 1;
+            } catch (FormatException) {
+                Console.Error.WriteLine("Could not parse input. Please only enter integral numbers. "
+                    + "See --help for details on which numbers can be formatted.");
+                return 1;
+            }
+        }
+    }
+}
---
+++ RomanNumerals.Console/Program.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace RomanNumerals.Console {
+    /// <summary>
+    /// The main entry point for the application.
+    /// This project uses the DragonFruit application model
+    /// (https://github.com/dotnet/command-line-api/wiki/DragonFruit-overview)
+    /// in order to provide a strongly-types main method.
+    /// </summary>
+    class Program
+    {
+        /// <summary>
+        /// Formats a number in roman numerals.
+        /// </summary>
+        /// <param name="argument">
+        /// The number to format. Valid numbers are between 1 and 3000, inclusively.
+        /// If no value is provided, it is read from stdin.
+        /// </param>
+        /// <returns>Exit Code 0 if the number was formatted successfully, Exit Code 1 otherwise.</returns>
+        static int Main(int? argument = null) {
+            var serviceCollection = new ServiceCollection();
+
+            ConfigureServices(serviceCollection);
+
+            return serviceCollection
+                .BuildServiceProvider()
+                .GetRequiredService<Application>()
+                .Run(argument);
+        }
+
+        private static void ConfigureServices(IServiceCollection serviceCollection)
+            => serviceCollection
+                .AddTransient<INumeralConverter, RomanNumeralConverter2>()
+                .AddSingleton<Application>()
+            ;
+    }
+}
